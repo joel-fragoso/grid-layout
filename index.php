39beb143233b11b61f8bfcc5063029621a4bbb9f
Joel Fragoso <joelfragoso85@icloud.com>
2020-03-02 17:10:17 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <!-- Meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="id=edge">
+  <meta author="Joel Fragoso">
+
+  <!-- Title -->
+  <title>Sirius - Grid Layout</title>
+
+  <!-- Stylesheet -->
+  <link rel="stylesheet" href="css/sirius.min.css">
+</head>
+<body>
+
+  <div class="container">
+    <div class="grid my-6 py-6">
+      <div class="col-2">
+        <div class="img">
+          <img class="img-resposive" src="img/2.jpg" alt="Image 01">
+        </div>
+      </div>
+      <div class="col-4">
+        <h1>Mensagem de Fé!</h1>
+        <p>Os bons dias lhe dão felicidade. Os maus dias lhe dão experiência. Ambos são essenciais para vida. A felicidade te faz doce, os problemas te mantém forte, a dor te mantém humano, as quedas te mantém humilde, o êxito te mantém brilhante. Mas só Deus te mantém de pé.</p>
+        <blockquote class="blockquote text-right">&mdash; mensagens10</blockquote>
+      </div>
+      <div class="col-2">
+        <div class="img">
+          <img class="img-resposive" src="img/2.jpg" alt="Image 01">
+        </div>
+      </div>
+      <div class="col-4">
+        <h1>Mensagem de Fé!</h1>
+        <p>Os bons dias lhe dão felicidade. Os maus dias lhe dão experiência. Ambos são essenciais para vida. A felicidade te faz doce, os problemas te mantém forte, a dor te mantém humano, as quedas te mantém humilde, o êxito te mantém brilhante. Mas só Deus te mantém de pé.</p>
+        <blockquote class="blockquote text-right">&mdash; mensagens10</blockquote>
+      </div>
+    </div>
+    
+    <div class="grid text-center py-2">
+      <div class="col-1 bg-primary">.col-1</div>
+      <div class="col-1 bg-primary">.col-1</div>
+      <div class="col-1 bg-primary">.col-1</div>
+      <div class="col-1 bg-primary">.col-1</div>
+      <div class="col-1 bg-primary">.col-1</div>
+      <div class="col-1 bg-primary">.col-1</div>
+      <div class="col-1 bg-primary">.col-1</div>
+      <div class="col-1 bg-primary">.col-1</div>
+      <div class="col-1 bg-primary">.col-1</div>
+      <div class="col-1 bg-primary">.col-1</div>
+      <div class="col-1 bg-primary">.col-1</div>
+      <div class="col-1 bg-primary">.col-1</div>
+    </div>
+    <div class="grid text-center pb-2">
+      <div class="col-2 bg-primary">.col-2</div>
+      <div class="col-2 bg-primary">.col-2</div>
+      <div class="col-2 bg-primary">.col-2</div>
+      <div class="col-2 bg-primary">.col-2</div>
+      <div class="col-2 bg-primary">.col-2</div>
+      <div class="col-2 bg-primary">.col-2</div>
+    </div>
+    <div class="grid text-center pb-2">
+      <div class="col-3 bg-primary">.col-3</div>
+      <div class="col-3 bg-primary">.col-3</div>
+      <div class="col-3 bg-primary">.col-3</div>
+      <div class="col-3 bg-primary">.col-3</div>
+    </div>
+    <div class="grid text-center pb-2">
+      <div class="col-4 bg-primary">.col-4</div>
+      <div class="col-4 bg-primary">.col-4</div>
+      <div class="col-4 bg-primary">.col-4</div>
+    </div>
+    <div class="grid text-center pb-2">
+      <div class="col-5 bg-primary">.col-5</div>
+      <div class="col-5 bg-primary">.col-5</div>
+      <div class="col-2 bg-primary">.col-2</div>
+    </div>
+    <div class="grid text-center pb-2">
+      <div class="col-12 bg-primary">.col-12</div>
+    </div>
+    <div class="grid text-center pb-2">
+      <div class="col-1 bg-primary">.col-1</div>
+      <div class="col-11 bg-primary">.col-11</div>
+    </div>
+    <div class="grid text-center pb-2">
+      <div class="col-2 bg-primary">.col-2</div>
+      <div class="col-10 bg-primary">.col-10</div>
+    </div>
+    <div class="grid text-center pb-2">
+      <div class="col-3 bg-primary">.col-3</div>
+      <div class="col-9 bg-primary">.col-9</div>
+    </div>
+    <div class="grid text-center pb-2">
+      <div class="col-4 bg-primary">.col-4</div>
+      <div class="col-8 bg-primary">.col-8</div>
+    </div>
+    <div class="grid text-center pb-2">
+      <div class="col-5 bg-primary">.col-5</div>
+      <div class="col-7 bg-primary">.col-7</div>
+    </div>
+    <div class="grid text-center pb-2">
+      <div class="col-6 bg-primary">.col-6</div>
+      <div class="col-6 bg-primary">.col-6</div>
+    </div>
+    <div class="grid text-center mb-6 pb-2">
+      <div class="offset-3 col-6 bg-primary">.col-6</div>
+    </div>
+  </div>
+  
+</body>
+</html>
